280cad37afba7ddbf2803728e2b98ac2966f5efc
dwing@163.com <dwing@163.com@d163a04c-1948-11df-bff1-5f4ea15f2b7c>
2010-02-14 11:23:46 +0000

    src: add lua-lab.txt, test and feature: - 支持 continue 语句, 作用和 C 相同, 可用在 for, while, repeat 循环中使用 - 支持 != 运算符, 作用和 ~= 相同

---
+++ trunk/test/lua-lab.lua
@@ -0,0 +1,52 @@
+﻿-- test UTF-8 BOM
+-- this file must be encoded by UTF-8 with BOM
+
+-- test wide-char variable name
+do
+	local 变量 = 123.456
+	print(变量 .. " = 123.456")
+end
+
+-- test continue statement
+do
+	local sum = 0
+	for i = 1, 10 do
+		if i == 5 then continue end
+		sum = sum + i
+	end
+	print(sum .. " = 50")
+
+	local sum = 0
+	local i = 0
+	while true do
+		i = i + 1
+		if i > 10 then break end
+		if i == 5 then continue end
+		sum = sum + i
+	end
+	print(sum .. " = 50")
+
+	local sum = 0
+	local i = 0
+	repeat
+		i = i + 1
+		if i == 5 then continue end
+		sum = sum + i
+		if i >= 10 then break end
+	until false
+	print(sum .. " = 50")
+end
+
+-- test != operator
+
+do
+	local i = 3
+	if i != 5 then
+		print "!= = ~="
+	else
+		print "!= != ~="
+	end
+end
+
+-- test hex-number in string (for Lua 5.2)
+print("\x41\x42\x43 = ABC")
